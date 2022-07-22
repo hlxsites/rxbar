cbdb90731eaeefee1da971a52612a89b4549da97
dyland <dylandepass@gmail.com>
2022-07-22 12:10:24 -0400

    feat: support text slides in carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,12 +1,23 @@
 export default function decorate(block) {
+  const slideStylesConfig = block.parentElement.parentElement.getAttribute('data-slidethemes');
+  const slideStyles = slideStylesConfig.replace(/, /g, ',').split(',');
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
   [...block.children].forEach((row, i) => {
-    const classes = ['image-desktop', 'image-mobile'];
-    classes.forEach((e, j) => {
-      if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
-      if (row.children[j]) row.children[j].classList.add(`slide`);
-    });
+    if (row.querySelector('img')) {
+      const classes = ['image-desktop', 'image-mobile'];
+      classes.forEach((e, j) => {
+        if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
+        if (row.children[j]) row.children[j].classList.add('slide');
+      });
+    } else {
+      const classes = ['text-slide'];
+      classes.forEach((e, j) => {
+        if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
+        if (row.children[j]) row.children[j].classList.add('slide');
+        if (i < slideStyles.length) row.classList.add(`slide-${slideStyles[i].toLowerCase()}`);
+      });
+    }
     /* buttons */
     const button = document.createElement('button');
     if (!i) button.classList.add('selected');
