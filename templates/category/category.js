2e6f7cf3266acd4f67736eaa57a14a5a674464c5
dyland <dylandepass@gmail.com>
2022-07-22 19:20:30 -0400

    fix: show products in category

--- templates/category/category.js
+++ templates/category/category.js
@@ -40,9 +40,9 @@ export default async function decorate(main) {
   const container = document.createElement('div');
   container.classList.add('category-container');
 
-  // products.forEach((product) => {
-  //   container.appendChild(renderProductCard(product));
-  // });
+  products.forEach((product) => {
+    container.appendChild(renderProductCard(product));
+  });
 
-  // main.appendChild(container);
+  main.appendChild(container);
 }
