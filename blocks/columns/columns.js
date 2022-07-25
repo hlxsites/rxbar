a0619464d7c238b53c8b2fb603ba054373b39a43
dyland <dylandepass@gmail.com>
2022-07-25 16:41:31 -0400

    fix: columns updates for marketing pages

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,9 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+
+  cols.forEach((col) => {
+    const anchor = col.querySelector('a');
+    console.log(anchor);
+  });
 }
