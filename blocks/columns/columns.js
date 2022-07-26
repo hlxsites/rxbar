d98902ef62f7e4d225db4d81c399f9f8f4089e4c
dyland <dylandepass@gmail.com>
2022-07-26 11:43:34 -0400

    fix: update gallery component for marketing pages

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,4 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-
-  cols.forEach((col) => {
-    const anchor = col.querySelector('a');
-    console.log(anchor);
-  });
 }
--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -24,45 +24,96 @@ class Gallery {
   constructor(block) {
     this.block = block;
     const imageContainers = block.querySelectorAll(':scope > div');
-    const title = getMetadata('og:title');
+    // If the block has rows
     if (imageContainers.length > 0) {
-      const slidesContainer = document.createElement('div');
-      slidesContainer.classList.add('slide-container');
-      const images = Array.from(imageContainers).map((container) => {
-        const imgSrc = container.querySelector('img').src;
-        container.classList.add('gallery-slide');
-        container.addEventListener('click', this.onSlideSelected);
-        slidesContainer.append(container);
-        const thumb = createOptimizedPicture(imgSrc, title, false, [{ width: '90' }]);
-        const img = thumb.querySelector('img');
-        img.width = 90;
-        img.height = 90;
-        container.textContent = '';
-        container.append(thumb);
-        return imgSrc;
-      });
+      // Create the slides element
+      const slides = this.createSlides(block);
+      block.append(slides);
 
-      block.append(slidesContainer);
+      // Select the first slide and build the selected slide element
+      const firstSlide = slides.querySelector('.gallery-slide:first-of-type');
+      firstSlide.classList.add('selected');
+      this.selectedSlide = firstSlide;
+      const firstImage = firstSlide.querySelector('img').src;
+
+      this.selectedSlideContainer = document.createElement('div');
+      this.selectedSlideContainer.classList.add('selected-slide');
 
       const galleryImageContainer = document.createElement('div');
       galleryImageContainer.classList.add('gallery-image');
-      this.galleryImage = document.createElement('img');
-      this.galleryImage.loading = 'eager';
-      const [mainImage] = images;
-      this.galleryImage.src = updateWidthParam(mainImage, 530);
-      galleryImageContainer.append(this.galleryImage);
-      block.prepend(galleryImageContainer);
+      const galleryImage = document.createElement('img');
+      galleryImage.loading = 'eager';
+      galleryImage.src = updateWidthParam(firstImage, 530);
+      galleryImageContainer.append(galleryImage);
+      this.selectedSlideContainer.appendChild(galleryImageContainer);
+
+      // If the first slide has content, add it to the selected slide
+      const firstContent = firstSlide.querySelector('.slide-content');
+      if (firstContent) {
+        this.selectedSlideContainer.appendChild(firstContent.cloneNode(true));
+      }
+
+      block.prepend(this.selectedSlideContainer);
     }
   }
 
+  createSlides(block) {
+    const title = getMetadata('og:title');
+    const slides = block.querySelectorAll(':scope > div');
+
+    const slidesContainer = document.createElement('div');
+    slidesContainer.classList.add('slide-container');
+
+    Array.from(slides).forEach((slide) => {
+      const cols = Array.from(slide.querySelectorAll(':scope > div'));
+
+      if (cols.length > 0) {
+        // First column should contain the slide image
+        const imgContainer = cols[0];
+        const imgSrc = imgContainer.querySelector('img').src;
+        const thumb = createOptimizedPicture(imgSrc, title, false, [{ width: '90' }]);
+        // Set explicity width/height on optimized img
+        const img = thumb.querySelector('img');
+        img.width = 90;
+        img.height = 90;
+        imgContainer.classList.add('gallery-slide');
+        imgContainer.addEventListener('click', this.onSlideSelected);
+        // Swap old img for optimized picture
+        imgContainer.textContent = '';
+        imgContainer.append(thumb);
+
+        // Is there content to go with the slide?
+        if (cols.length > 1) {
+          const slideContent = cols[1];
+          const slideContentContainer = document.createElement('div');
+          slideContentContainer.classList.add('slide-content');
+          cols[1].remove();
+          slideContentContainer.append(slideContent);
+          imgContainer.append(slideContentContainer);
+        }
+
+        slidesContainer.append(imgContainer);
+      }
+    });
+
+    return slidesContainer;
+  }
+
   onSlideSelected = (e) => {
+    const selectedImg = this.selectedSlideContainer.querySelector('img');
     if (this.selectedSlide) {
       this.selectedSlide.classList.remove('selected');
     }
 
     this.selectedSlide = e.target.closest('.gallery-slide');
     e.target.closest('.gallery-slide').classList.add('selected');
-    this.galleryImage.src = updateWidthParam(e.target.src, 530);
+    selectedImg.src = updateWidthParam(e.target.src, 530);
+
+    const content = this.selectedSlide.querySelector('.slide-content');
+    if (content) {
+      const contentElement = this.selectedSlideContainer.querySelector('.slide-content');
+      contentElement.innerHTML = content.innerHTML;
+    }
   };
 }
 
