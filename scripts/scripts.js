ade4198d8ccfb466ca9871d71b30abfde645088d
dyland <dylandepass@gmail.com>
2022-07-27 11:47:30 -0400

    fix: move category into lcpBlocks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -114,7 +114,7 @@ HelixApp.init({
   rumEnabled: true,
   autoAppear: true,
   rumGeneration: 'project-1',
-  lcpBlocks: ['carousel', 'responsive-image'],
+  lcpBlocks: ['carousel', 'category'],
 })
   .withLoadDelayed(() => {
     // eslint-disable-next-line import/no-cycle
