b07468c9184beb7f4fe02a342f0a44476441eb49
dyland <dylandepass@gmail.com>
2022-07-29 15:18:12 -0400

    fix: change graphql endpoint

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,7 +8,7 @@ sampleRUM('cwv');
 // add more delayed functionality here
 // Initialize storefront-sdk
 const options = {
-  endpoint: 'https://graph.adobe.io/api/63e62e43-8eb8-45a2-b0f6-f7c3845093db/graphql?api_key=2c6d06bb3aef463db8485c88a90f563f',
+  endpoint: 'https://helix-ystxy6i-egytbxb7cxneo.us-4.magentosite.cloud/graphql',
   mesh: 'storefrontstaticenvmesh',
 };
 render(options, document.body);
